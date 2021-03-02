a7155bea5183e0935d29c936972cf9dbdd21c7f1
Gustavo <gustavonikov@gmail.com>
2021-03-02 00:38:09 -0300

    Update: responsiveness

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -44,6 +44,7 @@ export default function CountdownProvider({ children }: CountdownProviderProps) 
             countdownTimeout = setTimeout(() => {
                setTime(time - 1);
            }, 1000)
+           
         } else if (isActive && time === 0) {
             setHasFinished(true);
             setIsActive(false);
